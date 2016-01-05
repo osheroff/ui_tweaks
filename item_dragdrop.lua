f5b4be805d924d99e71c17846e29992ba6415a16
Ben Osheroff <ben@zendesk.com>
2016-01-04 21:52:28 -0800

    checkpoint

--- item_dragdrop.lua
+++ item_dragdrop.lua
@@ -2,41 +2,37 @@ local upgradeScreen = include ( 'states/state-upgrade-screen' )
 local mui_defs = include("mui/mui_defs")
 local mui_dragzone = include( "mui/widgets/mui_dragzone" )
 local mui_image = include( "mui/widgets/mui_image" )
+local mui_button = include( "mui/widgets/mui_button" )
 local util = include( "modules/util" )
 local guiex = include( "client/guiex" )
 local cdefs = include( "client_defs" )
 
-function upgradeScreen:onDragInventory( upgrade, item, onDragDrop )
-  local widget = self.screen:startDragDrop( item, "DragItem" )
-  widget.binder.img:setImage( item:getUnitData().profile_icon )
-  self.screen:findWidget( "storageDragHilite" ):setColor( cdefs.COLOR_DRAG_DROP:unpack() )
-  self.screen:findWidget( "dragAugment" ).onDragDrop = function() util.coDelegate( self.onDragToAugments, self, upgrade, item ) end
-
-  self._preserved_inventory = {}
-  for i, widget in self.screen.binder:forEach( "inv_" ) do
-      table.insert(self._preserved_inventory, widget._item)
-  end
-
-  return true
-end
-
-local function updateButton(screen, widget, item)
+local function updateButton(screen, widget, item, encumbered)
     if item then
-        guiex.updateButtonFromItem( screen, nil, widget, item, nil, false )
+        guiex.updateButtonFromItem( screen, nil, widget, item, nil, encumbered )
     else
-        guiex.updateButtonEmptySlot( widget )
+        guiex.updateButtonEmptySlot( widget, encumbered )
     end
 end
 
-function upgradeScreen:onDragEnter( screen, idx )
+local function onDragDrop( self, item, upgrade )
+  repl:log("onDragDrop index == ", self.screen._lastDragIndex, " upgrade == ", upgrade)
+  return true
+end
+
+local function onDragOver( screen, idx )
+    if screen._lastDragIndex == idx then return true end
+
+    screen._lastDragIndex = idx
+
     local dragItem = screen:getDragDrop()
     repl:log("onDragEnter to " .. idx .. " with " .. tostring(dragItem))
 
     local inventory = {}
 
     for i, widget in screen.binder:forEach( "inv_" ) do
-        repl:log("widget " .. i .. " is " .. tostring(widget._item))
-        if widget._item and widget._item ~= dragItem then
+        -- repl:log("widget " .. i .. " is ", widget._item)
+        if widget._item and widget._item ~= dragItem and widget:getAlias() then -- getAlias ensures item still exists
             table.insert(inventory, widget._item)
         end
     end
@@ -48,7 +44,8 @@ function upgradeScreen:onDragEnter( screen, idx )
         local item = inventory[i]
         local widget = screen:findWidget( "inv_" .. i)
 
-        updateButton(screen, widget, item)
+        local encumbered = widget.binder.encumbered._cont._isVisible
+        updateButton(screen, widget, item, encumbered)
         if item and i == idx then
             widget.binder.btn:setColor(0.9, 0.75, 0.37, 1)
         end
@@ -57,17 +54,71 @@ function upgradeScreen:onDragEnter( screen, idx )
     return true
 end
 
+local function handleMouseOverDuringDrag( self, ev )
+  if ev.screen:getDragDrop() and ev.eventType == mui_defs.EVENT_MouseMove
+      and ev.screen:getInputLock() == self and self._prop:inside( ev.x, ev.y ) then
+    local xmin, ymin, zmin, xmax, ymax, zmax = self._prop:getWorldBounds()
 
-function upgradeScreen:onDragLeave( unitDef, agentIndex, dragIdx)
-    repl:log("onDragLeave from " .. dragIdx)
-    for i, item in ipairs(self._preserved_inventory) do
-        local widget = self.screen:findWidget( "inv_" .. i)
-        repl:log("resetting " .. i .. " to " .. tostring(item))
-        updateButton(self.screen, widget, item)
-        widget.binder.btn:setColor(0.95, 1, 0.47, 1)
+    local idx = math.floor((ev.x - xmin) / ((xmax - xmin) / 4)) + 1
+
+    if ev.y < ymin + ((ymax - ymin) / 2) then
+      idx = idx + 4
     end
-    return true
+
+    return onDragOver(ev.screen, idx)
+  else
+    return mui_button.handleInputEvent( self, ev )
+  end
 end
+
+function upgradeScreen:onDragEnter( dragWidget )
+  dragWidget._button.handleInputEvent = handleMouseOverDuringDrag
+  return true
+end
+
+local function saveInventory( screen )
+  local inv = {}
+
+  for i, widget in screen.binder:forEach( "inv_" ) do
+    local item = { encumbered = widget.binder.encumbered._cont._isVisible }
+    if widget:getAlias() then
+      item.item = widget._item
+    end
+
+    table.insert(inv, item)
+  end
+
+  return inv
+end
+
+local function restoreInventory( screen, inv )
+  for i, tbl in ipairs(inv) do
+    local widget = screen:findWidget( "inv_" .. i)
+    -- widget.binder.btn:setColor(0.95, 1, 0.47, 1)
+    updateButton( screen, widget, tbl.item, tbl.encumbered )
+  end
+end
+
+function upgradeScreen:onDragInventory( upgrade, item, _ )
+  self.screen._lastDragIndex = nil
+  local widget = self.screen:startDragDrop( item, "DragItem" )
+  widget.binder.img:setImage( item:getUnitData().profile_icon )
+
+  self._saved_inventory = saveInventory( self.screen )
+  self.screen:findWidget( "drag" ).onDragDrop = function(item) onDragDrop(self, item, upgrade) end
+  self.screen:findWidget( "storageDragHilite" ):setColor( cdefs.COLOR_DRAG_DROP:unpack() )
+  self.screen:findWidget( "dragAugment" ).onDragDrop = function() util.coDelegate( self.onDragToAugments, self, upgrade, item ) end
+
+  return true
+end
+
+function upgradeScreen:onDragLeave( dragWidget )
+  repl:log( "onDragLeave, restoring inventory" )
+  restoreInventory( self.screen, self._saved_inventory )
+  dragWidget._button.handleInputEvent = mui_button.handleInputEvent
+  return true
+end
+
 
 if not upgradeScreen.oldRefreshInventory then
     upgradeScreen.oldRefreshInventory = upgradeScreen.refreshInventory
@@ -76,86 +127,10 @@ end
 function upgradeScreen:refreshInventory( unitDef, index )
     self:oldRefreshInventory( unitDef, index )
 
-    self._preserved_inventory = {}
-
-    gUnitDef = unitDef
-    local oldDragWidget = self.screen:findWidget( "drag" )
-
- --   if oldDragWidget then
---        self.screen:removeWidget(oldDragWidget)
---    end
-
-    for i, widget in self.screen.binder:forEach( "inv_" ) do
-        local agentPnl = self.screen:findWidget( "agentPnl" )
-
-        local x = widget._cont._x
-        local y = widget._cont._y
-        local w = 58.5 / 1280
-        local h = 63
-
-        -- if i == 1 then
-          -- x = x + 4
-          -- w = w - 4
-        -- elseif i == 4 then
-          -- x = x - 4
-          -- w = w - 4
-        -- end
-
-        if not self.screen:findWidget( "drag_" .. i ) then
-            local dragzone_widget = mui_dragzone(self.screen, {
-                name = "drag_" .. i,
-                isVisible = true,
-                noInput = true,
-                x = x,
-                y = y,
-                w = w,
-                h = h,
-                xpx = true,
-                ypx = true,
-                wpx = false,
-                hpx = true,
-                sx = 1,
-                sy = 1,
-            })
-            dragzone_widget.onDragEnter = util.makeDelegate( self, 'onDragEnter', self.screen, i)
-            dragzone_widget.onDragLeave = util.makeDelegate( self, 'onDragLeave', unitDef, index, i)
-
-            agentPnl:addChild(dragzone_widget)
-            local img = mui_image(self.screen, {
-                name = "bg_" .. i,
-                noInput = true,
-                x = x,
-                y = y,
-                w = w,
-                h = h,
-                xpx = true,
-                ypx = true,
-                wpx = false,
-                hpx = true,
-                sx = 1,
-                sy = 1,
-                color =
-                {
-                    math.random(), math.random(), math.random(), 0.5,
-                },
-                images =
-                {
-                    {
-                        file = [[white.png]],
-                        name = [[]],
-                        color =
-                        {
-                            1,
-                            1,
-                            1,
-                            0.5,
-                        },
-                    },
-                }
-            })
-            agentPnl:addChild(img)
-        end
-    end
+    self._saved_inventory = {}
+    local dragWidget = self.screen:findWidget( "drag" )
+    dragWidget.onDragEnter = util.makeDelegate( self, 'onDragEnter', dragWidget )
+    dragWidget.onDragLeave = util.makeDelegate( self, 'onDragLeave', dragWidget )
 end
 
 function reload()
